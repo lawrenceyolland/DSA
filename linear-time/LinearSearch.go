d4db09b3349bacf86199190892d1cf993f511f56
Lawrence Yolland <lawrence.yolland@ericsson.com>
2023-04-16 22:53:07 +0100

    small formatting change

--- linear-time/LinearSearch.go
+++ linear-time/LinearSearch.go
@@ -11,15 +11,13 @@ func LinearSearch(haystack []int, needle int) bool {
 		}
 		iterCounter++
 	}
-
 	fmt.Printf("needle not found. iterations taken: %d\n", iterCounter)
 	return false
 }
 
 func main() {
 	var haystack = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	
+
 	LinearSearch(haystack, 6)
 	LinearSearch(haystack, 15)
-
 }
